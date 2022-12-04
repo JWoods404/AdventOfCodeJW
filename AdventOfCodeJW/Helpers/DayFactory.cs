f72695e84e85f55373aa88a8959a4201610e2fa4
Jeremy Woods <nihillasowagan@gmail.com>
2022-12-04 13:54:39 +0100

    chore: update numbering

--- AdventOfCodeJW/Helpers/DayFactory.cs
+++ AdventOfCodeJW/Helpers/DayFactory.cs
@@ -9,13 +9,15 @@ namespace AdventOfCodeJW.Helpers
             switch (dayId)
             {
                 case 1:
-                    return new Days.Day1.Day01(input);
+                    return new Days.Day01.Day01(input);
                 case 2:
-                    return new Days.Day2.Day02(input);
+                    return new Days.Day02.Day02(input);
                 case 3:
-                    return new Days.Day3.Day03(input);
+                    return new Days.Day03.Day03(input);
                 case 4:
-                    return new Days.Day4.Day04(input);
+                    return new Days.Day04.Day04(input);
+                case 5:
+                    return new Days.Day05.Day05(input);
             }
 
             return null;
